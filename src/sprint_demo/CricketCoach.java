796ec1be0cce03e1d1f2dfd2a384618ffe0bd2dd
Ana Li Oña Garcia <aonagarcia@gmail.com>
2020-06-01 19:37:04 -0500

    injeccion de valores

--- src/sprint_demo/CricketCoach.java
+++ src/sprint_demo/CricketCoach.java
@@ -2,6 +2,10 @@ package sprint_demo;
 
 public class CricketCoach implements Coach {
 	private FortuneService fortuneService;
+	
+	private String emailAddress;
+	private String team;
+	
 	//create no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
@@ -13,7 +17,27 @@ public class CricketCoach implements Coach {
 		System.out.println("CricketCoach: inside setter method -setFortuneService"); 
 		this.fortuneService = fortuneService;
 	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method -setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method -setTeam");
+		this.team = team;
+	}
 
 
 	@Override
